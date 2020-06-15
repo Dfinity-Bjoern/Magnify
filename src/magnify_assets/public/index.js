97a840eae89ea1e9370a784a2e50c4b0f369d616
Christoph <christoph.hegemann1337@gmail.com>
2020-06-15 15:56:12 +0200

    [wip] Working on setting up the rpcPeerConnection

--- src/magnify_assets/public/index.js
+++ src/magnify_assets/public/index.js
@@ -18,9 +18,51 @@ document.body.innerHTML = /*html*/`
   <ul id="offers"></ul>
   <h2>Answers:</h2>
   <ul id="answers"></ul>
+  <div>
+    <video id="localVideo" autoplay></video>
+    <video id="remoteVideo" autoplay></video>
+  </div>
 `;
 
-const sendOffer = recipient => magnify.offer(recipient, "sdp")
+let localStream;
+let localVideo = $("#localVideo")
+let remoteVideo = $("#remoteVideo")
+let isCaller;
+let rtcPeerConnection;
+let iceServers = { iceServers: [{ url: "stun:stun.services.mozilla.com" }] }
+
+const onAddStream = event => {
+  remoteVideo.src = URL.createObjectURL(event.stream)
+  remoteStream = event.stream
+}
+
+const onIceCandidate = event => {
+  console.log(`onIceCandidate: ${event}`)
+}
+
+const setLocalAndOffer = sessionDescription => {
+  rtcPeerConnection.setLocalDescription(sessionDescription)
+  magnify.offer(recipient, sessionDescription)
+}
+
+const sendOffer = recipient => {
+  // TODO(Christoph): video to true
+  navigator.mediaDevices.getUserMedia({ audio: true, video: false }).then(stream => {
+    localStream = stream;
+    localVideo.src = URL.createObjectURL(stream);
+    isCaller = true;
+  })
+  .then(() => {
+    rtcPeerConnection = new RTCPeerConnection(iceServers)
+    rtcPeerConnection.onicecandidate = onIceCandidate
+    rtcPeerConnection.onaddstream = onAddStream
+
+    rtcPeerConnection.addStream(localStream)
+    rtcPeerConnection.createOffer(setLocalAndOffer, e => console.log(e))
+  })
+  .catch(err => console.error(`Failed to connect 1: ${err}`))
+}
+
 const sendAnswer = initiator => magnify.answer(initiator, "answerSDP")
 
 $("#offerButton").addEventListener("click", ev => {
@@ -64,4 +106,3 @@ $("#listAnswersButton").addEventListener("click", ev => {
 magnify.ping().then(caller => {
   $("#callerId").innerText = `Hello ${caller._idHex}`
 })
-
