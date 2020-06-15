8d3f40033e69450f0a782e28bf06a63fe4f952ec
Christoph <christoph.hegemann1337@gmail.com>
2020-06-15 15:12:43 +0200

    UI for inputting principals
    Also allows listing offers as well as answers

--- src/magnify_assets/public/index.js
+++ src/magnify_assets/public/index.js
@@ -1,23 +1,58 @@
 import magnify from 'ic:canisters/magnify';
+import { CanisterId } from '@dfinity/agent';
 
-// magnify.greet(window.prompt("Enter your name:")).then(greeting => {
-//   window.alert(greeting);
-// });
+const $ = document.querySelector.bind(document);
+
+// Sadness
+const principalFromHex = hex => CanisterId.fromHex(hex)
+
+document.body.innerHTML = /*html*/`
+  <div id="callerId"> Hello (Loading ...)</div>
+  <label for=recipientInput>Recipient:</label>
+  <input id="recipientInput">
+  <button id="recipientButton" type="button">Call</button>
+  <button id="listOffersButton" type="button">List offers</button>
+  <button id="listAnswersButton" type="button">List answers</button>
+  <h2>Offers:</h2>
+  <ul id="offers"></ul>
+  <h2>Answers:</h2>
+  <ul id="answers"></ul>
+`;
+
+const sendOffer = recipient => magnify.offer(recipient, "sdp")
+
+$("#recipientButton").addEventListener("click", ev => {
+  const callerId = $("#recipientInput").value;
+  sendOffer(principalFromHex(callerId)).then(() => {
+    console.log(`Sent an offer to: ${callerId}`)
+  })
+})
+
+$("#listOffersButton").addEventListener("click", ev => {
+  const ul = $("#offers");
+  magnify.offers().then(offers => {
+    ul.textContent = '';
+    offers.forEach(offer => {
+      const newLi = document.createElement("li")
+      newLi.textContent = `${offer.initiator._idHex} => ${offer.recipient._idHex}`
+      ul.appendChild(newLi)
+    })
+  })
+})
+
+$("#listAnswersButton").addEventListener("click", ev => {
+  const ul = $("#answers");
+  magnify.answers().then(answers => {
+    ul.textContent = '';
+    answers.forEach(answer => {
+      const newLi = document.createElement("li")
+      newLi.textContent = `${answer.offer.initiator._idHex} => ${answer.offer.recipient._idHex}`
+      ul.appendChild(newLi)
+    })
+  })
+})
 
 magnify.ping().then(caller => {
-  document.body.innerHTML = `Hallo ${caller._idHex}`;
-  magnify.offer(caller, "sdp").then(
-    () => magnify.offers().then(
-      offers => {
-        console.log(offers);
-        magnify.answer(caller, "sdp answer").then(
-          () => {
-            magnify.offers().then(console.log);
-            magnify.answers().then(console.log);
-          }
-        )
-      }
-    )
-  )
+  $("#callerId").innerText = `Hello ${caller._idHex}`
 })
 
