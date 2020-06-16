1eb6cd8c03fee688891c9957d9c6890dadb68287
dprats <diego.prats@gmail.com>
2020-06-16 07:54:58 -0700

    created local html and css files for playing with css

--- src/magnify_assets/public/index.js
+++ src/magnify_assets/public/index.js
@@ -23,6 +23,24 @@ document.body.innerHTML = /*html*/`
     </div>
   </div>
   <br>
+  <h1>The flex-wrap Property</h1>
+
+  <p>The "flex-wrap: wrap;" specifies that the flex items will wrap if necessary:</p>
+  <div class="flex-container">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>  
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>  
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>  
+    <div>10</div>
+    <div>11</div>
+    <div>12</div>  
+  </div>
+
   <div id="roomControls">
     <label id="aliasInputLabel" for=aliasInput>Input your Alias:</label>
     <input id="aliasInput">
