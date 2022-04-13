57244740f2599cf57c13367560490a5dd40d067a
RJ <smith.rashan@gmail.com>
2022-04-13 18:22:33 -0400

    Upgrade project to dfx 0.8.4
    - Directory Restructure
    - Motoko syntax updates
    - Update config files and library imports
    - Update javascript to use dfinity principal and identity libraries
    - Rename canister

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,121 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const dfxJson = require("./dfx.json");
-// List of all aliases for canisters. This creates the module alias for
-// the `import ... from "ic:canisters/xyz"` where xyz is the name of a
-// canister.
-const aliases = Object.entries(dfxJson.canisters)
-  .reduce((acc, [name,value]) => {
-    const outputRoot = path.join(__dirname, dfxJson.defaults.build.output, name);
-    return {
-      ...acc,
-      ["ic:canisters/" + name]: path.join(outputRoot, name + ".js"),
-      ["ic:idl/" + name]: path.join(outputRoot, name + ".did.js"),
-    };
-  }, {});
-/**
- * Generate a webpack configuration for a canister.
- */
-function generateWebpackConfigForCanister(name, info) {
-  if (typeof info.frontend !== 'object') {
-    return;
+const CopyPlugin = require("copy-webpack-plugin");
+
+function initCanisterEnv() {
+  let localCanisters, prodCanisters;
+  try {
+    localCanisters = require(path.resolve(
+      ".dfx",
+      "local",
+      "canister_ids.json"
+    ));
+  } catch (error) {
+    console.log("No local canister_ids.json found. Continuing production");
   }
-  const inputRoot = __dirname;
-  return {
-    mode: "production",
-    entry: {
-      index: path.join(inputRoot, info.frontend.entrypoint),
-    },
-    devtool: "source-map",
-    optimization: {
-      // TODO Set to true before deployment
-      minimize: false,
-      minimizer: [new TerserPlugin()],
-    },
-    resolve: {
-      alias: aliases,
-    },
-    output: {
-      filename: "[name].js",
-      path: path.join(__dirname, info.frontend.output),
-    },
-    module: {
-      rules: [
-        { test: /\.css$/, use: ['style-loader','css-loader']  }
-      ]
-    },
-    plugins: [
-    ],
-  };
+  try {
+    prodCanisters = require(path.resolve("canister_ids.json"));
+  } catch (error) {
+    console.log("No production canister_ids.json found. Continuing with local");
+  }
+
+  const network =
+    process.env.DFX_NETWORK ||
+    (process.env.NODE_ENV === "production" ? "ic" : "local");
+
+  const canisterConfig = network === "local" ? localCanisters : prodCanisters;
+
+  return Object.entries(canisterConfig).reduce((prev, current) => {
+    const [canisterName, canisterDetails] = current;
+    prev[canisterName.toUpperCase() + "_CANISTER_ID"] =
+      canisterDetails[network];
+    return prev;
+  }, {});
 }
-// If you have webpack configurations you want to build as part of this
-// config, add them here.
-module.exports = [
-  ...Object.entries(dfxJson.canisters).map(([name, info]) => {
-    return generateWebpackConfigForCanister(name, info);
-  }).filter(x => !!x),
-];
+const canisterEnvVariables = initCanisterEnv();
 
+const isDevelopment = process.env.NODE_ENV !== "production";
 
+const frontendDirectory = "Magnify_assets";
 
+const asset_entry = path.join("src", frontendDirectory, "src", "index.html");
+
+module.exports = {
+  target: "web",
+  mode: isDevelopment ? "development" : "production",
+  entry: {
+    // The frontend.entrypoint points to the HTML file for this build, so we need
+    // to replace the extension to `.js`.
+    index: path.join(__dirname, asset_entry).replace(/\.html$/, ".js"),
+  },
+  devtool: isDevelopment ? "source-map" : false,
+  optimization: {
+    minimize: !isDevelopment,
+    minimizer: [new TerserPlugin()],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+    fallback: {
+      assert: require.resolve("assert/"),
+      buffer: require.resolve("buffer/"),
+      events: require.resolve("events/"),
+      stream: require.resolve("stream-browserify/"),
+      util: require.resolve("util/"),
+    },
+  },
+  output: {
+    filename: "index.js",
+    path: path.join(__dirname, "dist", frontendDirectory),
+  },
+
+  // Depending in the language or framework you are using for
+  // front-end development, add module loaders to the default
+  // webpack configuration. For example, if you are using React
+  // modules and CSS as described in the "Adding a stylesheet"
+  // tutorial, uncomment the following lines:
+  module: {
+   rules: [
+     { test: /\.(ts|tsx|jsx)$/, loader: "ts-loader" },
+     { test: /\.css$/, use: ['style-loader','css-loader'] }
+   ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, asset_entry),
+      cache: false,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, "src", frontendDirectory, "assets"),
+          to: path.join(__dirname, "dist", frontendDirectory),
+        },
+      ],
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
+      ...canisterEnvVariables,
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: [require.resolve("buffer/"), "Buffer"],
+      process: require.resolve("process/browser"),
+    }),
+  ],
+  // proxy /api to port 8000 during development
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": "/api",
+        },
+      },
+    },
+    hot: true,
+    watchFiles: [path.resolve(__dirname, "src", frontendDirectory)],
+    liveReload: true,
+  },
+};
